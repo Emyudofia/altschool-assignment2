8e949a54f3a1fdfd09f46a8b7efe1497285f5c4b
Emediong Udofia <emediongudofia751@gmail.com>
2022-10-28 23:32:55 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import {
+  Contact,
+  Home,
+  Navbar,
+  ErrorPage,
+  UserDetail,
+  Users,
+} from "./Components/Index";
+import { ThemeContext, theme } from "./Context";
+// import Dynamic from "./Components/Dynamic";
+// import UserDetail from "./Components/UserDetails";
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+}
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const color = darkMode ? theme.dark : theme.light;
+  const handleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-body">
+      <ThemeContext.Provider value={theme}>
+        <Navbar
+          darkMode={darkMode}
+          color={color}
+          handleTheme={handleTheme}
+          setDarkMode={setDarkMode}
+        />
+        <Routes>
+          <Route path="/" element={<Home darkMode={darkMode} />} />
+          <Route path="/users" element={<Users darkMode={darkMode} />}>
+            <Route
+              path=":username"
+              element={
+                <ErrorBoundary FallBackComponent={ErrorFallback}>
+                  <UserDetail darkMode={darkMode} />
+                </ErrorBoundary>
+              }
+            />
+          </Route>
+          <Route path="/contact" element={<Contact darkMode={darkMode} />} />
+          <Route path="*" element={<ErrorPage darkMode={darkMode} />} />
+        </Routes>
+      </ThemeContext.Provider>
     </div>
   );
 }
